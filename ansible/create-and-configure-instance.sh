55d7d08fae6dcd60abca5abf1181733c9a67b613
Danielyuuuuu <48498200+Danielyuuuuu@users.noreply.github.com>
2021-05-03 00:13:53 +1000

    add proxy still doesn't work

--- ansible/create-and-configure-instance.sh
+++ ansible/create-and-configure-instance.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-. ./unimelb-comp90024-2021-grp-7-openrc.sh; ansible-playbook create-and-configure-instance.yaml
+. ./unimelb-comp90024-2021-grp-7-openrc.sh; ansible-playbook --ask-become-pass create-and-configure-instance.yaml
