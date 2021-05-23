cb71e24005316a17e3531b1cef5ec8a862036e2b
Rex Ding <rexding@outlook.com>
2021-05-23 20:00:41 +0800

    clean structure

--- example ansible/deploy_backend.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-. ./unimelb-comp90024-2021-grp-7-openrc.sh; ansible-playbook --ask-become-pass deploy_backend.yaml -i inventory/application_hosts.ini
--- example ansible/remove_deploy_instances.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-. ./unimelb-comp90024-2021-grp-7-openrc.sh; ansible-playbook --ask-become-pass remove_deploy_instances.yaml
