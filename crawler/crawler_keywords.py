29f42abb96aa72296dfaf6a4648d19a872b373ea
Windwalker1314 <60423153+Windwalker1314@users.noreply.github.com>
2021-05-23 20:11:08 +1000

    Crawler Split data by keywords

--- crawler/crawler_keywords.py
+++ crawler/crawler_keywords.py
@@ -1,9 +1,4 @@
-words = {'vaccine':['jab','vaccine','vaccination','AstraZeneca','Pfizer-BioNTech','vacc'],
-'lockdown':['lockdown'],
-'covid':['covid', 'covid19','coronavirus']
+words = {'vaccine':['jab','vaccine','vaccination','astraZeneca','afizer-biontech','vacc'],
+'covid':['lockdown','covid', 'covid19','coronavirus','cov19','covid-19','cov-19']
 }
-words = {'vaccine':['jab','vaccine','vaccination','AstraZeneca','Pfizer-BioNTech','vacc'],
-'lockdown':['lockdown'],
-'covid':['covid', 'covid19','coronavirus']
-}
-words_arr=['jab','vaccine','vaccination','astraZeneca','afizer-bioNTech','vacc','lockdown','covid', 'covid19','coronavirus','cov19','covid-19','cov-19']
+words_arr=['jab','vaccine','vaccination','astraZeneca','afizer-biontech','vacc','lockdown','covid', 'covid19','coronavirus','cov19','covid-19','cov-19']
